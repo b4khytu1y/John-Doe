091d332b59be5a4a361fe2982450eb3e9aed3e12
Backender <mrvibe25@gmail.com>
2021-12-16 14:21:30 +0600

    Animated

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,93 @@
 $(document).ready(function() {   
 
-const body = document.querySelector('body')
-cx = window.innerWidth / 2;
-cy = window.innerHeight / 2;
+    const body = document.querySelector('body')
 
-    body.addEventListener('mousemove', e => {
+	cx = window.innerWidth / 2
+	cy = window.innerHeight / 2
 
-        clientX = e.pageX;
-        clientY = e.pageY;
+	body.addEventListener('mousemove', e => {
 
-        request = requestAnimationFrame(updateMe)
-    })
+		clientX = e.pageX
+		clientY = e.pageY
 
-    function updateMe() {
-        dx = clientX - cx
-        dy = clientY - cy
-        tiltx = dy/cy
-        tilty = dx/cx
+		request = requestAnimationFrame(updateMe)
 
-        radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
-        degree = radius * 24
+		mouseCoords(e)
+		cursor.classList.remove('hidden')
+		follower.classList.remove('hidden')
 
-        gsap.to('.main-information', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )`})
-    }
+	})
+
+	function updateMe() {
+
+		dx     = clientX - cx
+		dy     = clientY - cy
+		tiltx  = dy / cy
+		tilty  = dx / cx
+		radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
+		degree = radius * 12
+		gsap.to('.text-inform', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
+
+	}
+
+
+	const cursor   = document.getElementById('cursor'),
+				follower = document.getElementById('aura'),
+				links    = document.getElementsByTagName('a')
+
+	mouseX = 0, mouseY = 0, posX = 0, posY = 0
+
+	function mouseCoords(e) {
+
+		mouseX = e.pageX
+		mouseY = e.pageY
+
+	}
+
+	gsap.to({}, .01, {
+
+		repeat: -1,
+
+		onRepeat: () => {
+
+			posX += (mouseX - posX) / 5
+			posY += (mouseY - posY) / 5
+
+			gsap.set(cursor, {
+				css: {
+					left: mouseX,
+					top: mouseY
+				}
+			})
+
+			gsap.set(follower, {
+				css: {
+					left: posX - 23,
+					top: posY - 23
+				}
+			})
+
+		}
+
+	})
+
+	for(let i = 0; i < links.length; i++) {
+
+		links[i].addEventListener('mouseover', () => {
+			cursor.classList.add('active')
+			follower.classList.add('active')
+		})
+
+		links[i].addEventListener('mouseout', () => {
+			cursor.classList.remove('active')
+			follower.classList.remove('active')
+		})
+
+	}
+
+	body.addEventListener('mouseout', () => {
+		cursor.classList.add('hidden')
+		follower.classList.add('hidden')
+	})
+    
 });
